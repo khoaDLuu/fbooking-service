3ed7928aab767e7156675f9bbf83b223c5de47ef
Keith Dluu <dangkhoa240899@gmail.com>
2021-05-13 01:37:53 +0700

    Config service for deployment with config server and discovery server

--- src/main/java/com/filmbooking/booking_service/BookingServiceApplication.java
+++ src/main/java/com/filmbooking/booking_service/BookingServiceApplication.java
@@ -3,11 +3,13 @@ package com.filmbooking.booking_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication( exclude = {SecurityAutoConfiguration.class} )
 @Import(BSSecurityConfig.class)
+@EnableEurekaClient
 @EnableJpaAuditing
 public class BookingServiceApplication {
 
