12d8cafe840365af3afbe298c73e88af3c698a8b
Keith Dluu <dangkhoa240899@gmail.com>
2021-06-01 08:36:48 +0700

    Add a simple implementation for User

---
+++ src/main/java/com/filmbooking/booking_service/utils/user/Requester.java
@@ -0,0 +1,17 @@
+package com.filmbooking.booking_service.utils.user;
+
+public class Requester implements User {
+
+    @Override
+    public String name() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String roles() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
